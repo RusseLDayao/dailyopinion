a823689cb2ea683086f5b53f253920ff46ad82d1
RusseLDayao <russeldayao@gmail.com>
2024-02-06 14:57:04 +0800

    First commit

---
+++ app/layout.jsx
@@ -0,0 +1,18 @@
+import '@styles/global.css';
+
+export const metadata= {
+    title: "Daily Opinion",
+    description: "Share your thoughs on our current events world wide with anyone"
+}
+
+const layout = () => {
+  return (
+    <html>
+        <body>
+            
+        </body>
+    </html>
+  )
+}
+
+export default layout
