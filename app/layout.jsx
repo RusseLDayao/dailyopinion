32cd91ed64f0ef0b12157816b456141df8076bb9
RusseLDayao <russeldayao@gmail.com>
2024-02-06 16:44:15 +0800

    second commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,18 +1,24 @@
 import '@styles/global.css';
+import { Children } from 'react';
 
 export const metadata= {
     title: "Daily Opinion",
     description: "Share your thoughs on our current events world wide with anyone"
 }
 
-const layout = () => {
+const layout = (Children) => {
   return (
-    <html>
+    <html lang='en'>
         <body>
-            
+            <div className='main'>
+                <div className='gradient'/>
+            </div>
+            <main className='app'>
+                {Children}
+            </main>
         </body>
     </html>
   )
 }
 
-export default layout
+export default RootLayout;
